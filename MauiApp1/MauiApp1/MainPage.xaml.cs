be251d3973b9c68c24c5cbf607e5f67136c0d397
Karina-Abjima <karina.gwalani@abjima.com>
2023-06-19 18:23:29 +0530

    basic app with a button and redirecting to new page

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+//	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnOpenVideoPageClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new VideoPage());
+    }
+}
---
+++ MauiApp1/MauiApp1/VideoPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1;
+
+public partial class VideoPage : ContentPage
+{
+	
+    public VideoPage()
+    {
+        InitializeComponent();
+        LoadVideo();
+    }
+
+    private void LoadVideo()
+    {
+        string youtubeVideoUrl = "https://www.youtube.com/embed/Hh279ES_FNQ";
+        myWebView.Source = new UrlWebViewSource { Url = youtubeVideoUrl };
+    }
+}
